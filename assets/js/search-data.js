70440549f8ccade83464bace6105532fcf689c59
YY-GX <32232491+YY-GX@users.noreply.github.com>
2025-06-25 21:53:42 +0000

    Deploying to gh-pages from @ YY-GX/YY-GX.github.io@3ad4981e443bb61d659fc4cf91feff87b7efbefd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,17 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-boom-one-paper-accepted-by-ieee-ra-l",
+            },},{id: "news-boom-one-paper-accepted-by-alt-hri-2025",
+          title: ':boom: One paper accepted by alt.HRI 2025.',
+          description: "",
+          section: "News",},{id: "news-boom-one-paper-accepted-by-ieee-ra-l",
           title: ':boom: One paper accepted by IEEE RA-L.',
           description: "",
           section: "News",},{id: "news-boom-one-paper-accepted-by-ieee-rsj-international-conference-on-intelligent-robots-and-systems-iros-2025",
           title: ':boom: One paper accepted by IEEE/RSJ International Conference on Intelligent Robots and Systems...',
+          description: "",
+          section: "News",},{id: "news-i-present-our-iros-24-paper-at-abu-dhabi-uae",
+          title: '💬 I present our IROS’24 paper at Abu Dhabi, UAE!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
